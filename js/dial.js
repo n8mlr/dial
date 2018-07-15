5249ca32f5075afc9d6965040ffe99e8eaa1f741
Nate Miller <nate@natemiller.org>
2018-07-14 19:42:11 -0700

    minor

--- js/dial.js
+++ js/dial.js
@@ -35,11 +35,14 @@ class Dial {
         let c = this.canvas.getContext('2d');
         let theta = Math.PI * 2 / menuItems.length;
         var opts = this.options();
-        var item;
+        
 
         c.beginPath();
 
         // draw lines first, rotate if needed, then draw menu items..
+        var item,
+            tmLabel,
+            tmIcon;
 
         for (var i = 0; i < menuItems.length; i++) {
             item = menuItems[i];
@@ -48,14 +51,14 @@ class Dial {
             if (item.name) {
                 c.fillStyle = item.color;
                 c.font = "500 24px museo-sans-rounded";
-                var tmLabel = c.measureText(item.name);
+                tmLabel = c.measureText(item.name);
                 c.fillText(item.name, pt.x - tmLabel.width / 2, opts.txtLabelOffsetRatio * radius);
             }
 
             // draw icon
             if (item.iconName) {
                 c.font = "72px unearth";
-                var tmIcon = c.measureText(item.iconName);
+                tmIcon = c.measureText(item.iconName);
                 c.fillText(item.iconName, pt.x - tmIcon.width / 2, opts.iconOffsetRaio * radius);
             }
 
